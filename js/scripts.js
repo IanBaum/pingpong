4713fb62ff91f5d5eeeb47cebd2a85d67b93721f
Ian Baum <ian@ianbaum.com>
2016-08-19 08:41:31 -0400

    update logic to print pong when number is divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       if(i % 3 === 0){
         $("#outputList").append("<li> ping </li>");
       }
-      else if(i === 5){
+      else if(i % 5 === 0){
         $("#outputList").append("<li> pong </li>");
       }
       else{
