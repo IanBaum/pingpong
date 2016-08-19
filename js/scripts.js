3f7b2c6ac96a41231ac090c47d601363aa6bda28
Ian Baum <ian@ianbaum.com>
2016-08-19 08:36:09 -0400

    add buisness logic to print list up to users number

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 
   var pingPong = function(number){
-    
+    for(i=1;i<=number;i++){
+      $("#outputList").append("<li>" + i + "</li>");
+    }
   }
 
   $("#userInput").submit(function(event){
     event.preventDefault();
     var number= parseInt($("#userNumber").val());
+    $("#outputList").text("");
     pingPong(number);
   })
 });
