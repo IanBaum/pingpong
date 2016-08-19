2b29e49efd7b3ec234abb228a46d312bb19afe59
Ian Baum <ian@ianbaum.com>
2016-08-19 08:44:20 -0400

    udate logic to print pingpong when number is divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
 
   var pingPong = function(number){
     for(i=1;i<=number;i++){
-      if(i % 3 === 0){
+      if(i % 3 === 0 && i % 5 != 0){
         $("#outputList").append("<li> ping </li>");
       }
-      else if(i % 5 === 0){
+      else if(i % 5 === 0 && i % 3 != 0){
         $("#outputList").append("<li> pong </li>");
+      }
+      else if(i % 5 === 0 && i % 3 === 0){
+        $("#outputList").append("<li> pingpong </li>");
       }
       else{
         $("#outputList").append("<li>" + i + "</li>");
