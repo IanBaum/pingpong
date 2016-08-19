7b02e2b0d0a23d9098c95c02643361d5e3deab8e
Ian Baum <ian@ianbaum.com>
2016-08-19 12:06:07 -0400

    add images and badger function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  var button = 0;
 
   var pingPong = function(number){
     for(i=1;i<=number;i++){
@@ -16,11 +17,38 @@ $(document).ready(function(){
       }
     }
   }
-
+  var badgerMushroom = function(number){
+    for(i=1;i<=number;i++){
+      if(i % 3 === 0 && i % 5 != 0){
+        $("#outputList").append("<li><img src='img/mushroom.png'></li>");
+      }
+      else if(i % 5 === 0 && i % 3 != 0){
+        $("#outputList").append("<li><img src='img/mushroom.png'></li>");
+      }
+      else if(i % 5 === 0 && i % 3 === 0){
+        $("#outputList").append("<li><img src='img/snake.png'></li>");
+      }
+      else{
+        $("#outputList").append("<li><img src='img/badger.png'></li>");
+      }
+    }
+  }
+  $("#mainButton").click(function(){
+    button = 0;
+  });
+  $("#badgerButton").click(function(){
+    button = 1;
+  });
   $("#userInput").submit(function(event){
     event.preventDefault();
     var number= parseInt($("#userNumber").val());
     $("#outputList").text("");
-    pingPong(number);
+    if(button === 0){
+      pingPong(number);
+    }
+    else if (button === 1){
+      $("#outputList").text("");
+      badgerMushroom(number);
+    }
   })
 });
